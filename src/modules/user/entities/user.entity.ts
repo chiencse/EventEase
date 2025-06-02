c5a3b90be4ada6954fb5a6d12bf61af67a1a237e
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2025-06-02 23:43:17 +0700

    feat/suggestion friend

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,39 +1,37 @@
-import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { AuditEntity } from 'src/common/entities/audit.entity';
 
-
 @Entity('users')
 export class User extends AuditEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ name: 'first_name', length: 50 })
-    firstName: string;
+  @Column({ name: 'first_name', length: 50 })
+  firstName: string;
 
-    @Column({ name: 'last_name', length: 50 })
-    lastName: string;
+  @Column({ name: 'last_name', length: 50 })
+  lastName: string;
 
-    @Column({ name: 'date_of_birth', type: 'date', nullable: true })
-    dateOfBirth: Date;
+  @Column({ name: 'date_of_birth', type: 'date', nullable: true })
+  dateOfBirth: Date;
 
-    @Column({ unique: true })
-    email: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column({ nullable: true })
-    location: string;
+  @Column({ nullable: true })
+  location: string;
 
-    @Column({ name: 'phone_number', unique: true, length: 20, nullable: true })
-    phoneNumber: string;
+  @Column({ name: 'phone_number', unique: true, length: 20, nullable: true })
+  phoneNumber: string;
 
-    @Column({ nullable: true })
-    avatar: string;
+  @Column({ nullable: true })
+  avatar: string;
 
-    @Column({ unique: true })
-    username: string;
+  @Column({ unique: true })
+  username: string;
 
-    @Column()
-    @Exclude()
-    password: string;
-
-} 
+  @Column()
+  @Exclude()
+  password: string;
+}
